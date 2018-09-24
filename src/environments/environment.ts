69a7f46a220613d7f527d61ae70acab007b44724
Nahuel Reymundo <32850187+NahuelReymundo@users.noreply.github.com>
2018-09-24 12:41:59 -0300

    Subida

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBW73Xc7i7FoHnvvuOU9SVs4Z8-T1iE4eI",
+    authDomain: "estacionmeteorologica-fe823.firebaseapp.com",
+    databaseURL: "https://estacionmeteorologica-fe823.firebaseio.com",
+    projectId: "estacionmeteorologica-fe823",
+    storageBucket: "estacionmeteorologica-fe823.appspot.com",
+    messagingSenderId: "600082090709"
+  }
+};
